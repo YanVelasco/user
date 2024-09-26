a42c6cbce007fb4b3533b084c5ab594db3e04d34
Yan Velasco <yancarlosduartevelasco@gmail.com>
2024-09-26 11:34:49 -0300

    feet: save user

--- src/main/java/com/yanvelasco/domain/userapi/entities/AddressEntity.java
+++ src/main/java/com/yanvelasco/domain/userapi/entities/AddressEntity.java
@@ -17,19 +17,23 @@ public class AddressEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @Column(name = "street")
     private String street;
+
     @Column(name = "number")
     private Long number;
+
     @Column(name = "complement", length = 10)
     private String complement;
+
     @Column(name = "city", length = 150)
     private String city;
+
     @Column(name = "state", length = 2)
     private String state;
+
     @Column(name = "zip_code", length = 9)
     private String zip_code;
-    @Column(name = "user_id")
-    private UUID user_id;
 
 }
